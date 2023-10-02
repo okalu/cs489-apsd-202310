4f94f84ca10eb2858e0df41af780ad37edde5ff3
Prof OKalu <okalu1@outlook.com>
2023-10-02 13:54:58 -0500

    Added Lab1a solution

---
+++ LabSolutions/Lab1a/task4/ProductMgmtApp/src/edu/miu/cs/cs489appsd/lab1a/productmgmtapp/ProductMgmtApp.java
@@ -0,0 +1,8 @@
+package edu.miu.cs.cs489appsd.lab1a.productmgmtapp;
+
+public class ProductMgmtApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello CS489");
+    }
+}
