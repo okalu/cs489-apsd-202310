879205cbd4c9b7ef60b8129dbcd9205fd07958c4
Prof OKalu <okalu1@outlook.com>
2023-10-03 14:54:08 -0500

    Updated Lesson1b code examples

--- Lessons/lesson1b-build-automation/CustomerMgmtCLIApp/src/main/java/edu/miu/cs/cs489/lesson1b/custimermgmtcliapp/CustomerMgmtApp.java
+++ Lessons/lesson1b-build-automation/CustomerMgmtCLIApp/src/main/java/edu/miu/cs/cs489/lesson1b/custimermgmtcliapp/CustomerMgmtApp.java
@@ -1,2 +1,13 @@
-package edu.miu.cs.cs489.lesson1b.custimermgmtcliapp.model;public class CustomerMgmtApp {
+package edu.miu.cs.cs489.lesson1b.custimermgmtcliapp;
+
+public class CustomerMgmtApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Apache Maven");
+    }
+
+    public String sayHello() {
+        return "Hello";
+    }
+
 }
--- Lessons/lesson1b-build-automation/CustomerMgmtCLIApp/src/main/java/edu/miu/cs/cs489/lesson1b/custimermgmtcliapp/model/Customer.java
+++ Lessons/lesson1b-build-automation/CustomerMgmtCLIApp/src/main/java/edu/miu/cs/cs489/lesson1b/custimermgmtcliapp/model/Customer.java
@@ -1,2 +1,14 @@
-package edu.miu.cs.cs489.lesson1b.custimermgmtcliapp;public class Customer {
+package edu.miu.cs.cs489.lesson1b.custimermgmtcliapp.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Customer {
+    private Integer customerId;
+    private String name;
+
 }
